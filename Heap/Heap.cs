0ab1ed271f7c0f302cae8512ce75a772e9472967
IloPaTa69 <pavelchash@list.ru>
2024-12-15 22:22:18 +0300

    Добавьте файлы проекта.

---
+++ Heap/Heap.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programm
+{
+    public class Heap
+    {
+        private List<int> heap;
+
+        public Heap()
+        {
+            heap = new List<int>();
+        }
+
+        private int Parent(int index) => (index - 1) / 2;
+        private int LeftChild(int index) => 2 * index + 1;
+        private int RightChild(int index) => 2 * index + 2;
+
+        private void SiftUp(int index)
+        {
+            while (index > 0 && heap[Parent(index)] > heap[index])
+            {
+                Swap(index, Parent(index));
+                index = Parent(index);
+            }
+        }
+
+        private void SiftDown(int index)
+        {
+            int minIndex = index;
+            int left = LeftChild(index);
+            int right = RightChild(index);
+
+            if (left < heap.Count && heap[left] < heap[minIndex])
+                minIndex = left;
+            if (right < heap.Count && heap[right] < heap[minIndex])
+                minIndex = right;
+
+            if (index != minIndex)
+            {
+                Swap(index, minIndex);
+                SiftDown(minIndex);
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            int temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;
+        }
+
+        public void Add(int value)
+        {
+            heap.Add(value);
+            SiftUp(heap.Count - 1);
+        }
+
+        public int RemoveMin()
+        {
+            if (heap.Count == 0)
+                return int.MaxValue;
+
+            int minValue = heap[0];
+            heap[0] = heap[heap.Count - 1];
+            heap.RemoveAt(heap.Count - 1);
+
+            if (heap.Count > 0)
+                SiftDown(0);
+
+            return minValue;
+        }
+
+        public void DecreaseKey(int index, int newValue)
+        {
+            if (index < 0 || index >= heap.Count)
+                return;
+
+            if (newValue > heap[index])
+                return;
+
+            heap[index] -= newValue;
+            SiftUp(index);
+        }
+
+        public void IncreaseKey(int index, int newValue)
+        {
+            if (index < 0 || index >= heap.Count)
+                return;
+
+            if (newValue < heap[index])
+                return;
+
+            heap[index] += newValue;
+            SiftDown(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", heap);
+        }
+    }
+}
---
+++ Heap/Program.cs
@@ -0,0 +1,22 @@
+﻿using Programm;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Heap heap = new Heap(); 
+        heap.Add(10);
+        heap.Add(5);
+        heap.Add(20);
+        heap.Add(3);
+
+        Console.WriteLine("Минимум: " + heap.RemoveMin());  // Ожидается 3
+        Console.WriteLine("Куча после удаления минимума: " + heap);
+
+        heap.DecreaseKey(1, 4);  // Уменьшаем элемент
+        Console.WriteLine("Куча после уменьшения элемента: " + heap);
+
+        heap.IncreaseKey(0, 15);  // Увеличиваем элемент
+        Console.WriteLine("Куча после увеличения элемента: " + heap);
+    }
+}
